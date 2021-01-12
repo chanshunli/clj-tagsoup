783b4825e4a370ca7c9592dc06aa0eac0b982c49
stevechan <chanshunli@gmail.com>
2021-01-13 00:50:24 +0800

    update

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject clj-tagsoup "0.4.1"
+(defproject steve-clj-tagsoup "0.4.2"
   :description "A HTML parser for Clojure."
+  :deploy-repositories [["releases" :clojars]
+                       ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.ccil.cowan.tagsoup/tagsoup "1.2.1"]
