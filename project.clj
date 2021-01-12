bd3cb93ed716bcbc14c3cab90360dda2504362d0
stevechan <chanshunli@gmail.com>
2021-01-12 16:22:50 +0800

    更新

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-tagsoup "0.4.0"
+(defproject clj-tagsoup "0.4.1"
   :description "A HTML parser for Clojure."
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojars.nathell/tagsoup "1.2.1"]
+                 [org.ccil.cowan.tagsoup/tagsoup "1.2.1"]
                  [net.java.dev.stax-utils/stax-utils "20070216"]])
